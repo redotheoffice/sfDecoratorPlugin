c1c2200f5516666217137725b7cc172eb21db647
Sjoerd de Jong <sjoerd@weett.nl>
2009-12-12 11:09:42 +0800

    Removed conditionally loading of autoloader

--- config/sfDecoratorPluginConfiguration.class.php
+++ config/sfDecoratorPluginConfiguration.class.php
@@ -7,15 +7,12 @@ class sfDecoratorPluginConfiguration extends sfPluginConfiguration
 {
   public function configure()
   {
-    if (sfConfig::get('app_sfDecoratorPlugin_generate_on_demand', false))
+    if (class_exists('sfDecoratorAutoload'))
     {
-      if (class_exists('sfDecoratorAutoload'))
-      {
-        // sfPluginConfiguration::configure is run several times each request, the first time
-        // the sfAutoloaderSimple has not been started, so only when sfDecoratorAutoload comes
-        // available, it should be registered
-        sfDecoratorAutoload::getInstance()->register($this->configuration);
-      }
+      // sfPluginConfiguration::configure is run several times each request, the first time
+      // the sfAutoloaderSimple has not been started, so only when sfDecoratorAutoload comes
+      // available, it should be registered
+      sfDecoratorAutoload::getInstance()->register($this->configuration);
     }
   } 
 }
