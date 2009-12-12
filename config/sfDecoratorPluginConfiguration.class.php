ce081d816548aa2e423a5cfad775ac188bd8b00b
Sjoerd de Jong <sjoerd@weett.nl>
2009-12-12 11:13:39 +0800

    Added forced inclusion of autoloader class

--- config/sfDecoratorPluginConfiguration.class.php
+++ config/sfDecoratorPluginConfiguration.class.php
@@ -7,12 +7,9 @@ class sfDecoratorPluginConfiguration extends sfPluginConfiguration
 {
   public function configure()
   {
-    if (class_exists('sfDecoratorAutoload'))
-    {
-      // sfPluginConfiguration::configure is run several times each request, the first time
-      // the sfAutoloaderSimple has not been started, so only when sfDecoratorAutoload comes
-      // available, it should be registered
-      sfDecoratorAutoload::getInstance()->register($this->configuration);
-    }
+    // class might not be loaded yet, force the load
+    require_once(dirname(__FILE__).'/../lib/sfDecoratorAutoload.class.php');
+    
+    sfDecoratorAutoload::getInstance()->register($this->configuration);
   } 
 }
